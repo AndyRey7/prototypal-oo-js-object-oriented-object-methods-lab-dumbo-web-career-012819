430d93da720928fd477cbc4e9bf40f8a6cb2a5d2
Andy Reyes <11710reyes@gmail.com>
2019-09-21 18:53:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-class BoardMember {
-  constructor(name, homeState, training) {
+function BoardMember(name, homeState, training) {
     this.name = name
     this.homeState = homeState
     this.training = training
+  
+  BoardMember.prototype.veto = () => {
+    return "No, I must disagree"
   }
   
-  BoardMember.prototype.veto = function() {
-    return 
+  BoardMember.prototype.approve = () => {
+    return "You can do it!"
+  }
+  
+  BoardMember.prototype.doCharity = () => {
+    return "I like to help people"
+  }
+  
+  BoardMember.prototype.releasePressStatement = () => {
+    return "You will see great things from Scruber"
+  }
+  
+  BoardMember.prototype.sayHi = () => {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 
 }
